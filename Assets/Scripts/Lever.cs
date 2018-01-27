7aaf860453cb749d38582f1d55a00e77fd48674f
Pegasus <orumcan@gmail.com>
2018-01-27 18:56:10 +0300

    Act 1 map is totally completed.

---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Lever : MonoBehaviour 
+{
+    [SerializeField]
+    private Animator doorAnimator;
+    [SerializeField]
+    private bool shouldStay;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == Tags.player || other.tag == Tags.cube)
+        {
+            doorAnimator.SetBool("isTriggered", true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (shouldStay)
+        {
+            if (other.tag == Tags.player || other.tag == Tags.cube)
+            {
+                doorAnimator.SetBool("isTriggered", false);
+            }
+        }
+    }
+}
